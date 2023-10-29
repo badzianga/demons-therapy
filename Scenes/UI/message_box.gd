54f737047342ecc9fdb707aa499ce0c588c36e37
Tomasz Oszczypko <tomekoszczypko@gmail.com>
2023-10-29 16:35:43 +0100

    Changed all fonts

--- Scenes/UI/message_box.gd
+++ Scenes/UI/message_box.gd
@@ -3,9 +3,11 @@ extends TextureRect
 
 func display(type: String) -> void:
 	if type == "found":
+		$AnimationPlayer.speed_scale = 1.0
 		$LabelBigger.text = "You found your gold!"
 		$LabelSmaller.text = "Now survive until 6 AM..."
 	elif type == "start":
-		$LabelBigger.text = "Find your gold!"
+		$AnimationPlayer.speed_scale = 0.75
+		$LabelBigger.text = "Find your gold before 6 AM!"
 		$LabelSmaller.text = "Find a shovel and dig up the piles of dirt."
 	$AnimationPlayer.play("move")
